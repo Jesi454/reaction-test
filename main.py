7b144a167499d188a922ae976d1e27bca869e521
Jessica Leighton <91185461+Jesi454@users.noreply.github.com>
2022-06-13 10:10:47 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,39 +1,54 @@
 def progressImg():
     global num
     num += 1
-    num = num % 2
+    num = num % 4
+    images2[num].show_image(0)
+
+def on_button_pressed_a():
+    basic.show_number(fastestTime)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
 timeTaken = 0
 endTime = 0
 startTime = 0
 start = 0
+fastestTime = 0
+images2: List[Image] = []
+num = 0
+num = 0
 running = 0
-num = 0
-num = 0
 goImg = images.create_image("""
-    # # # # #
+    . . . . #
         # . . . #
+        # . # . #
         # . . . #
-        # . . . #
-        # # # # #
+        # . . . .
 """)
 Img2 = images.create_image("""
-    . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
+    . . # # #
+        . . . . #
+        . . # . .
+        # . . . .
+        # # # . .
 """)
 img3 = images.create_image("""
-    . . . . .
+    # # # # .
         . . . . .
         . . # . .
         . . . . .
-        . . . . .
+        . # # # #
 """)
-images2 = [goImg, Img2, img3]
+img4 = images.create_image("""
+    # # . . .
+        # . . . .
+        # . # . #
+        . . . . #
+        . . . # #
+""")
+images2 = [goImg, Img2, img3, img4]
 
 def on_forever():
-    global start, running, startTime, endTime, timeTaken, num
+    global start, running, startTime, endTime, timeTaken, fastestTime, num
     if not (running):
         start = randint(0, 10)
         if start == 1:
@@ -45,8 +60,10 @@ def on_forever():
         timeTaken = endTime - startTime
         basic.show_number(timeTaken)
         basic.pause(5000)
-        running = 0
+        if timeTaken > fastestTime:
+            fastestTime = timeTaken
         num = 0
+        running = 0
     else:
         progressImg()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,36 +1,58 @@
 function progressImg () {
     num += 1
-    num = num % 3
+    num = num % 4
+    images2[num].showImage(0)
 }
+input.onButtonPressed(Button.A, function () {
+    if (fastestTime == 0) {
+        basic.showString("CHEATER")
+    } else {
+        basic.showNumber(fastestTime)
+    }
+})
 let timeTaken = 0
 let endTime = 0
 let startTime = 0
 let start = 0
-let running = 0
+let fastestTime = 0
+let images2: Image[] = []
 let num = 0
 num = 0
+let running = 0
 let goImg = images.createImage(`
-    # # # # #
     # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
     # . . . #
-    # . . . #
-    # # # # #
     `)
 let Img2 = images.createImage(`
-    . . . . .
+    . . . # .
+    # . # . .
     . # # # .
-    . # . # .
-    . # # # .
-    . . . . .
+    . . # . #
+    . # . . .
     `)
 let img3 = images.createImage(`
-    . . . . .
-    . . . . .
     . . # . .
-    . . . . .
-    . . . . .
+    . . # . .
+    # # # # #
+    . . # . .
+    . . # . .
     `)
-let images2 = [goImg, Img2, img3]
+let img4 = images.createImage(`
+    . # . . .
+    . . # . #
+    . # # # .
+    # . # . .
+    . . . # .
+    `)
+images2 = [
+goImg,
+Img2,
+img3,
+img4
+]
 basic.forever(function () {
     if (!(running)) {
         start = randint(0, 10)
@@ -42,13 +64,15 @@ basic.forever(function () {
     }
     if (input.buttonIsPressed(Button.B)) {
         endTime = input.runningTime()
-        timeTaken = endTime - startTime
+        timeTaken = (endTime - startTime) % 1000 % 60
         basic.showNumber(timeTaken)
         basic.pause(5000)
-        running = 0
+        if (timeTaken > fastestTime) {
+            fastestTime = timeTaken
+        }
         num = 0
+        running = 0
     } else {
         progressImg()
-        images2[num].showImage(0)
     }
 })
